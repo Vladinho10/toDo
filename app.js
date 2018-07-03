95077e7fff083df85d751fc80bf2ab8fd4afaf82
gitUserVlad <vlad.mkhitaryan10@gmail.com>
2018-07-03 16:14:13 +0400

    to do is done

---
+++ app.js
@@ -0,0 +1,81 @@
+const bodyParser = require("body-parser")
+, cookieParser = require("cookie-parser")
+, express = require("express")
+, app = express()
+, _port = 3000;
+
+// console.log(__dirname+"/public");
+app.use(express.static(__dirname+"/public"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.set('view engine', 'pug');
+
+let toDoList = [ ];
+
+app.get('/', (req, res) => {
+  let page = 'form';
+  res.render(page, {
+    toDosArr: toDoList
+  } )
+  console.log('request');
+});
+
+let id = 0;
+app.post('/form', (req, res)=>{
+  if( req.body.toDoText ) {
+    let obj = {
+      str: req.body.toDoText,
+      id: id++
+    }
+    toDoList.push(obj)
+  };
+  res.redirect('/')
+});
+
+app.post('/edit/:id', (req, res)=>{
+  // console.log(req.params);
+  // console.log(req.params.id);
+  let elem = toDoList.find((el) => {
+    return el.id==req.params.id
+  })
+  console.log(elem);
+  console.log('elemmmmmmmmm');
+  res.render('edit', {
+    obj: elem,
+    title: 'edit'
+  })
+
+});
+
+app.post('/demo/:id', (req, res)=>{
+  console.log('demo00000000000');
+  console.log(req.body);
+  console.log(req.params.id);
+  let elem = toDoList.find((el) => {
+    return el.id==req.params.id
+  });
+  elem.str = req.body.toDoText;
+  console.log(elem);
+  console.log('saveeeeeeeeeeeee');
+  let num = toDoList.indexOf(elem);
+  // console.log(num+'num');
+  // toDoList.splice(num, 1, req.body);
+  res.redirect('/')
+});
+
+app.post('/del/:id', (req, res)=>{
+  let elem = toDoList.find((el) => {
+    return el.id==req.params.id
+  })
+  console.log('deeeelll');
+  console.log(elem);
+  let num = toDoList.indexOf(elem);
+  console.log(num+'num');
+  toDoList.splice(num, 1);
+  res.redirect('/')
+});
+
+app.listen(_port, () => console.log(`${_port}`));
---
+++ public/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+console.log(16);
+
+addBtn.addEventListener('click', (e)=>{
+    // console.log(toDoText.value);
+    //
+    // let xhr = new XMLHttpRequest();
+    // xhr.open('GET', 'localhost:3000/form', true);
+    // xhr.send();
+
+}, );
+
+
+// if( toDoText.value ) {
+//   let li = document.createElement('li');
+//   li.innerHTML = toDoText.value;
+//   ol.appendChild(li);
+// };
+// toDoText.value = ''
